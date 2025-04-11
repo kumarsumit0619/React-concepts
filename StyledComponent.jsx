597317cc88789f05bd437a291d9c8a2079dc1703
Sumit Kumar <54531986+kumarsumit0619@users.noreply.github.com>
2025-04-11 13:24:46 +0530

    Create StyledComponent.jsx

---
+++ StyledComponent.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledComponent = () => {
+  const [name, setName] = useState('');
+  const [tag, setTag] = useState('No Tag Found');
+  console.log('StyledComponent executed');
+
+  const inputChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    debugger;
+    event.preventDefault();
+    console.log('submit executed');
+    setTag(name);
+    setName('');
+  };
+
+  const StyledForm = styled.form`
+    font-size: 25px;
+    padding: 5px;
+    margin: 2px;
+    background: green;
+
+    &:hover,
+    &:active{
+      background: red;
+    }
+
+    & label{
+      color: #fff;
+    }
+  `;
+
+  const StyledSubmitbutton = styled.button`
+  background-color: violet;
+  font-size: 18px;
+  padding: 5px;
+  margin: 10px;
+  border-radius: 4px;
+  color:#fff;
+  border: none;
+  
+  &:hover{
+    background: blue;
+    color: #fff;
+  }
+  `;
+
+  return (
+    <>
+      <StyledForm onSubmit={formSubmitHandler}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={inputChangeHandler}
+            id="name"
+          />
+        </div>
+        <StyledSubmitbutton type="submit">Submit</StyledSubmitbutton>
+      </StyledForm>
+      <div>Tags: {tag}</div>
+    </>
+  );
+};
+
+export default StyledComponent;
