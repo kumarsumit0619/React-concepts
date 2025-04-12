2ba76eb85d4f704ed5f829ba1c12a34ffbf67651
Sumit Kumar <54531986+kumarsumit0619@users.noreply.github.com>
2025-04-12 11:26:30 +0530

    Create App.jsx

---
+++ Portals/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './style.css';
+
+import Toast from './components/Toast';
+
+export default function App() {
+  const [toastMessage, setToastMessage] = useState(null);
+
+  const showToast = () => {
+    setToastMessage('Records updated!!!');
+    setTimeout(() => setToastMessage(null), 300000);
+  };
+  
+  return (
+      <div>
+        <button className="btn" onClick={showToast}>
+          Show Toast notification
+        </button>
+        {toastMessage && (
+          <Toast message={toastMessage} onClose={() => setToastMessage(null)} />
+        )}
+      </div>
+  );
+}
